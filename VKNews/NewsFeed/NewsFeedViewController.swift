a7356517c53a7f0bda6ab7ff7ecad00d0ccd3ed6
Grigoriev Pavel <p.s.grigoriev@yandex.ru>
2019-07-23 09:26:22 +0300

    Resized post icons

--- VKNews/NewsFeed/NewsFeedViewController.swift
+++ VKNews/NewsFeed/NewsFeedViewController.swift
@@ -49,7 +49,7 @@ class NewsFeedViewController: UIViewController, NewsFeedDisplayLogic {
     super.viewDidLoad()
     setup()
     setupTable()
-    view.backgroundColor = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)
+    view.backgroundColor = #colorLiteral(red: 0, green: 0.5898008943, blue: 1, alpha: 1)
     
     interactor?.makeRequest(request: .getNewsFeed)
   }
